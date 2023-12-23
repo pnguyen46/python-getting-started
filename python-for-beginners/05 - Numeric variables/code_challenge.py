c4ca4ec4d25c32ebd91a91e37f12ccdde5dc1ff3
Tron Nguyen <pnguye894@gmail.com>
2023-12-23 14:59:56 -0500

    number

--- python-for-beginners/05 - Numeric variables/code_challenge.py
+++ python-for-beginners/05 - Numeric variables/code_challenge.py
@@ -1,6 +1,13 @@
 # Ask a user to enter a number
+first_number = input("Please enter a number: ")
+
 # Ask a user to enter a second number
+second_number = input("Please enter another number: ")
+
 # Calculate the total of the two numbers added together
+total = int(first_number) + int(second_number)
 # Print 'first number + second number = answer' 
 # For example if someone enters 4 and 6 the output should read
 # 4 + 6 = 10
+output = f'{first_number} + {second_number} = {total}'
+print(output)
--- python-for-beginners/05 - Numeric variables/convert_strings_to_numbers_for_math.py
+++ python-for-beginners/05 - Numeric variables/convert_strings_to_numbers_for_math.py
@@ -1,8 +1,11 @@
+#input function will return a string
 first_num = input('Enter first number ')
 second_num = input('Enter second number ')
+
 # If you have a string variable containing a number
 # And you want to treat it as a number
 # You must convert it to a numeric datatype
+
 # int() converts a string to an integer e.g. 5, 8, 416, 506
 print(int(first_num) + int(second_num))
 
