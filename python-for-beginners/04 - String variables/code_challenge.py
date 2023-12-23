9871ec5eb23a152071d84157757aa8a0b3bdfdbc
Tron Nguyen <pnguye894@gmail.com>
2023-12-23 14:40:33 -0500

    string

--- python-for-beginners/04 - String variables/code_challenge.py
+++ python-for-beginners/04 - String variables/code_challenge.py
@@ -1,6 +1,11 @@
 # ask a user to enter their first name and store it in a variable
+first_name = input("Please enter your first name: ")
 # ask a user to enter their last name and store it in a variable
+last_name = input("Please enter your last name: ")
 # print their full name
 # Make sure you have a space between first and last name
+print("Hello " + first_name + ' ' + last_name)
+
 # Make sure the first letter of first name and last name is uppercase
 # Make sure the rest of the name is lowercase
+print("Hello " + first_name.capitalize() + ' ' + last_name.capitalize())
--- python-for-beginners/04 - String variables/format_strings.py
+++ python-for-beginners/04 - String variables/format_strings.py
@@ -6,3 +6,10 @@ last_name = input('What is your last name? ')
 # the first letter uppercase and the rest of the word lowercase
 print ('Hello ' + first_name.capitalize() + ' ' \
        + last_name.capitalize())
+
+# Ways to format string:
+# output = 'Hello, {} {}'.format(first_name,last_name)
+# output = 'Hello, {0} {1}'.format(first_name,last_name)
+# output = 'Hello {1}, {0}'.format(first_name,last_name)
+output = f'Hello, {first_name} {last_name}'
+print(output)
