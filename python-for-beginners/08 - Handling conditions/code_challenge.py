0ba1e02cdfc5d4ce2ab38663c3bc41696efe028e
Tron Nguyen <pnguye894@gmail.com>
2023-12-25 12:46:59 -0500

    conditional basic

--- python-for-beginners/08 - Handling conditions/code_challenge.py
+++ python-for-beginners/08 - Handling conditions/code_challenge.py
@@ -2,11 +2,13 @@
 # If I enter 2.00 I should see the message "Tax rate is: 0.07" 
 # If I enter 1.00 I should see the message "Tax rate is: 0.07" 
 # If I enter 0.50 I should see the message "Tax rate is: 0" 
-price = input('how much did you pay? ')
 
-if price > 1.00:
-	tax = .07
-	print('Tax rate is: ' + str(tax))
-else
+price = input('how much did you pay? ')
+price = float(price)
+if price >= 1.00:
+	tax = 0.07
+else:
 	tax = 0
 print('Tax rate is: ' + str(tax))
+
+
