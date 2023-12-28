321b86e79e29927b9b645cb0c7fc97d92186997f
Tron Nguyen <pnguye894@gmail.com>
2023-12-28 17:35:11 -0500

    and operator

--- python-for-beginners/10 - Complex conditon checks/code_challenge.py
+++ python-for-beginners/10 - Complex conditon checks/code_challenge.py
@@ -1,9 +1,9 @@
 # When you join a hockey team you get your name on the back of the jersey
 # but the jersey may not be big enough to hold all the letters
 # Ask the user for their first name
-
+first_name = input('Enter your first name: ')
 # Ask the user for their last name
-
+last_name = input('Enter your last name')
 # if first name is < 10 characters and last name is < 10 characters 
 #       print first and last name on the jersey
 # if first name >= 10 characters long and last name is < 10 characters
@@ -12,7 +12,7 @@
 #       print entire first name and first initial of last name
 # if first name >= 10 characters long and last name is >= 10 characters
 #       print last name only
-
+#
 # Test with the following values
 # first name: Susan  last name: Ibach
 # output: Susan Ibach
@@ -22,3 +22,22 @@
 # output: R. Ibach
 # first name: ReallyLongFirstName  last name: ReallyLongLastName
 # output: ReallyLongLastName
+if len(first_name) >= 10:
+    long_first_name = True
+else:
+    long_first_name = False
+if len(last_name) >= 10:
+    long_last_name = True
+else:
+    long_last_name = False
+
+if long_first_name and long_last_name:
+    print(last_name)
+elif long_first_name:
+    print(first_name[0:1] + '. ' + last_name)
+elif long_last_name:
+    print(first_name + ' ' + last_name[0:1] + '.')
+else:
+    print(first_name + ' ' + last_name)
+
+
