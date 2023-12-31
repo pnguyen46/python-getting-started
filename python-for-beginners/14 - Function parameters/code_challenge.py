5d2fa9e14c2a1df4e04fd66b8f28900e0012ff79
Tron Nguyen <pnguye894@gmail.com>
2023-12-31 13:25:48 -0500

    function parameters

--- python-for-beginners/14 - Function parameters/code_challenge.py
+++ python-for-beginners/14 - Function parameters/code_challenge.py
@@ -14,3 +14,14 @@
 # 
 # BONUS: Test your function with the values 6, 4 and divide 
 # Have your function return an error message when invalid values are received
+def calculator(first_number, second_number, operation='ADD'):
+    if operation.upper() == 'ADD':
+        return(float(first_number) + float(second_number))
+    elif operation.upper() =='SUBTRACT':
+        return(float(first_number) - float(second_number))
+    else:
+        return('Invalid operation please specify ADD or SUBTRACT')
+
+print('Adding 6 + 4 = ' + str(calculator(first_number=6, second_number=4)))
+
+print('Subtracting 6 - 4 = ' + str(calculator(first_number=6, second_number=4, operation='subtract')))
